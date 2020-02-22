db957411edb3e23c8b9deb8934d361a1fdd107a4
Jan Studeny <jendas1@yahoo.com>
2020-02-22 22:38:29 +0000

    Update

---
+++ faces.py
@@ -0,0 +1,78 @@
+import os
+import argparse
+import pickle
+from tqdm import tqdm
+import PIL.Image
+import numpy as np
+import dnnlib
+import dnnlib.tflib as tflib
+import config
+from encoder.generator_model import Generator
+from encoder.perceptual_model import PerceptualModel
+
+URL_FFHQ = 'https://drive.google.com/uc?id=1MEGjdvVpUsu1jB4zrXZN7Y4kBBOzizDQ'  # karras2019stylegan-ffhq-1024x1024.pkl
+
+tflib.init_tf()
+with dnnlib.util.open_url(URL_FFHQ, cache_dir=config.cache_dir) as f:
+    generator_network, discriminator_network, Gs_network = pickle.load(f)
+
+generator = Generator(Gs_network, batch_size=1, randomize_noise=False)
+
+
+smile_direction = np.load('ffhq_dataset/latent_directions/smile.npy')
+gender_direction = np.load('ffhq_dataset/latent_directions/gender.npy')
+age_direction = np.load('ffhq_dataset/latent_directions/age.npy')
+
+
+def split_to_batches(l, n):
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
+
+
+def latent_representation(image,image_size=256,learning_rate=1,iterations=1000,randomize_noise=False):
+    ref_images = [image]
+    batch_size = 1
+    tflib.init_tf()
+    perceptual_model = PerceptualModel(image_size, layer=9, batch_size=batch_size)
+    perceptual_model.build_perceptual_model(generator.generated_image)
+
+    # Optimize (only) dlatents by minimizing perceptual loss between reference and generated images in feature space
+    for images_batch in tqdm(split_to_batches(ref_images, batch_size), total=len(ref_images) // batch_size):
+        perceptual_model.set_reference_images(images_batch)
+        op = perceptual_model.optimize(generator.dlatent_variable, iterations=iterations, learning_rate=learning_rate)
+        pbar = tqdm(op, leave=False, total=iterations)
+        for loss in pbar:
+            pbar.set_description(' Loss: %.2f' % loss)
+        print(' ', ' loss:', loss)
+        generated_dlatents = generator.get_dlatents()
+        generator.reset_dlatents()
+    return generated_dlatents
+
+
+
+def morph_latent(alpha,move_strength=1.0,steps=10,me_pt=None,direction=smile_direction+age_direction):
+    z = np.empty((steps, 18, 512))
+    for i, cur_alpha in enumerate(np.linspace(start=alpha, stop=alpha+move_strength, num=steps)):
+        z[i] = me_pt + direction * alpha
+    return z
+
+
+def generate_gif(latent_vectors,filename):
+    images = generate_images(latent_vectors)
+    # Save into a GIF file that loops forever
+    images[0].save(filename, format='GIF', append_images=images[1:], save_all=True, duration=40, loop=1)
+
+def generate_images(latent_vectors):
+    generator.set_dlatents(latent_vectors)
+    img_array = generator.generate_images()
+    imgs = []
+    for img in img_array:
+        imgs.append(PIL.Image.fromarray(img_array, 'RGB'))
+    return imgs
+
+def generate_image(latent_vector):
+    latent_vector = latent_vector.reshape((1, 18, 512))
+    generator.set_dlatents(latent_vector)
+    img_array = generator.generate_images()[0]
+    img = PIL.Image.fromarray(img_array, 'RGB')
+    return img
