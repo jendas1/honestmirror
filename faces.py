7699c6b1d049016a262789b9f1778f275d6a2c26
Jan Studeny <jendas1@yahoo.com>
2020-02-23 10:03:13 +0000

    2

--- faces.py
+++ faces.py
@@ -29,29 +29,31 @@ def split_to_batches(l, n):
         yield l[i:i + n]
 
 
-def latent_representation(image,learning_rate=1,iterations=50):
+def latent_representation(image,image_size=256,learning_rate=1,iterations=50,randomize_noise=False):
     ref_images = [image]
     print(ref_images)
     batch_size = 1
     tflib.init_tf()
+    #with dnnlib.util.open_url(URL_FFHQ, cache_dir=config.cache_dir) as f:
+    #    generator_network, discriminator_network, Gs_network = pickle.load(f)
+
+    #generator = Generator(Gs_network, batch_size, randomize_noise=randomize_noise)
+    perceptual_model = PerceptualModel(image_size, layer=9, batch_size=batch_size)
+    perceptual_model.build_perceptual_model(generator.generated_image)
 
     # Optimize (only) dlatents by minimizing perceptual loss between reference and generated images in feature space
     for images_batch in tqdm(split_to_batches(ref_images, batch_size), total=len(ref_images) // batch_size):
-        latent_representation.perceptual_model.set_reference_images(images_batch)
-        op = latent_representation.perceptual_model.optimize(latent_representation.generator.dlatent_variable, iterations=iterations, learning_rate=learning_rate)
+        perceptual_model.set_reference_images(images_batch)
+        op = perceptual_model.optimize(generator.dlatent_variable, iterations=iterations, learning_rate=learning_rate)
         pbar = tqdm(op, leave=False, total=iterations)
         for loss in pbar:
             pbar.set_description(' Loss: %.2f' % loss)
         print(' ', ' loss:', loss)
-        generated_dlatents = latent_representation.generator.get_dlatents()
-        latent_representation.generator.reset_dlatents()
+        generated_dlatents = generator.get_dlatents()
+        generator.reset_dlatents()
     return generated_dlatents
 
-with dnnlib.util.open_url(URL_FFHQ, cache_dir=config.cache_dir) as f:
-    latent_representation.generator_network, latent_representation.discriminator_network, latent_representation.Gs_network = pickle.load(f)
-latent_representation.generator = Generator(latent_representation.Gs_network, 1, randomize_noise=False)
-latent_representation.perceptual_model = PerceptualModel(256, layer=9, batch_size=1)
-latent_representation.perceptual_model.build_perceptual_model(latent_representation.generator.generated_image)
+
 
 def morph_latent(alpha,move_strength=1.0,steps=10,me_pt=None,direction=smile_direction+age_direction):
     z = np.empty((steps, 18, 512))
